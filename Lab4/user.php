412ba880a68efe1bd0f3d59d584b96bf3ea59777
eliobch123 <47105737+eliobch123@users.noreply.github.com>
2020-10-02 11:58:25 +0300

    Create user.php

---
+++ Lab4/user.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> </title>
+  </head>
+  <body>
+  <form action="">
+    <label>Introduction
+        <textarea name="comments" rows="6" cols="30" >
+        </textarea>
+    </label>
+  </form>
+    
+  </body>
+</html>
