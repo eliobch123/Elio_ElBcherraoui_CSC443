9ef855778d2a3e82a5f87949fd7bfce09c786b09
eliobch123 <47105737+eliobch123@users.noreply.github.com>
2020-10-30 12:14:43 +0200

    Update search.php

--- Lab7/search.php
+++ Lab7/search.php
@@ -7,7 +7,7 @@ $account_username=$_POST["username"];
 $db_host="localhost";
 $db_user="root";
 $db_pass=null;
-$db_name="mydatabase";
+$db_name="lab7";
 
 $mysqli=new mysqli($db_host, $db_user, $db_pass, $db_name);
 
